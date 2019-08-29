67371e8a145870c4f99e5309187626cc9eff52b2
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-08-28 18:13:41 -0600

    Update current dates

--- src/scripts.js
+++ src/scripts.js
@@ -16,31 +16,31 @@ $(document).ready(() => {
   $('.p__span--email').text(user.email);
   $('.p__span--stride').text(user.strideLength);
   $('.section__p--step-goals').text(userRepository.compareGoals(randomUserId));
-  $('.p__span--daily-water').text(hydrationUser.findDailyHydration('2019/06/15', randomUserId));
+  $('.p__span--daily-water').text(hydrationUser.findDailyHydration('2019/09/22', randomUserId));
   $('.article__p--weekly').text(hydrationUser.findWeeklyHydration(randomUserId));
-  $('.p__span--sleep-hours').text(sleepUser.findDailySleep('2019/06/15', randomUserId));
-  $('.p__span--sleep-quality').text(sleepUser.findDailySleepQuality('2019/06/15', randomUserId));
-  $('.p__span--sleep-average-hours').text(sleepUser.findAverageHoursSlept('2019/06/15', '2019/06/21', randomUserId));
-  $('.p__span--sleep-average-quality').text(sleepUser.findAverageQualitySlept('2019/06/15', '2019/06/21', randomUserId));
-  $('.section__p--weekly-sleep-hours').text(sleepUser.findWeeklyHoursSlept('2019/06/15', '2019/06/21', randomUserId));
-  $('.section__p--weekly-sleep-quality').text(sleepUser.findWeeklySleepQuality('2019/06/15', '2019/06/21', randomUserId));
+  $('.p__span--sleep-hours').text(sleepUser.findDailySleep('2019/09/22', randomUserId));
+  $('.p__span--sleep-quality').text(sleepUser.findDailySleepQuality('2019/09/22', randomUserId));
+  $('.p__span--sleep-average-hours').text(sleepUser.findAverageHoursSlept('2019/09/16', '2019/09/22', randomUserId));
+  $('.p__span--sleep-average-quality').text(sleepUser.findAverageQualitySlept('2019/09/16', '2019/09/22', randomUserId));
+  $('.section__p--weekly-sleep-hours').text(sleepUser.findWeeklyHoursSlept('2019/09/16', '2019/09/22', randomUserId));
+  $('.section__p--weekly-sleep-quality').text(sleepUser.findWeeklySleepQuality('2019/09/16', '2019/09/22', randomUserId));
   // $('.header__date').text(findTodaysDate());
   $('.article__p--steps-daily').text(activityUser.getDailyStepCount(randomUserId));
-  $('.article__p--minutes-daily').text(activityUser.findMinutesActive('2019/06/15', randomUserId));
-  $('.section__p--miles-daily').text(activityUser.calculateMilesWalked('2019/06/15', randomUserId));
-  $('.article__span--steps-weekly').text(activityUser.findAverageMinutesActive('2019/06/15', '2019/06/21', randomUserId));
+  $('.article__p--minutes-daily').text(activityUser.findMinutesActive('2019/09/22', randomUserId));
+  $('.section__p--miles-daily').text(activityUser.calculateMilesWalked('2019/09/22', randomUserId));
+  $('.article__span--steps-weekly').text(activityUser.findAverageMinutesActive('2019/09/16', '2019/09/22', randomUserId));
   $('.article__p--percent-world').text(activityUser.calculatePercentOfWorldWalked(randomUserId));
-  $('.article__h4--weekly-steps').text(activityUser.getWeeklyStepCount('2019/06/15', randomUserId));
+  $('.article__h4--weekly-steps').text(activityUser.getWeeklyStepCount('2019/09/22', randomUserId));
 
-  $('.section__p--compare-step').text(activityUser.compareStepAverageWithUser('2019/06/15', randomUserId))
-  $('.section__p--compare-minutes').text(activityUser.compareMinutesAverageWithUser('2019/06/15', randomUserId))
-  $('.section__p--compare-stairs').text(activityUser.compareStairsAverageWithUser('2019/06/15', randomUserId))
-  $('.test').text(activityUser.showUserFriendsSteps("2019/06/23", randomUserId))
-  $('.test2').text(activityUser.showUserFriends("2019/06/23", randomUserId))
+  $('.section__p--compare-step').text(activityUser.compareStepAverageWithUser('2019/09/22', randomUserId))
+  $('.section__p--compare-minutes').text(activityUser.compareMinutesAverageWithUser('2019/09/22', randomUserId))
+  $('.section__p--compare-stairs').text(activityUser.compareStairsAverageWithUser('2019/09/22', randomUserId))
+  $('.test').text(activityUser.showUserFriendsSteps('2019/09/22', randomUserId))
+  $('.test2').text(activityUser.showUserFriends('2019/09/22', randomUserId))
 
-  $('.li__span--total-steps').text(activityUser.calculateWeeksSteps('2019/06/15', '2019/06/21', randomUserId));
-  $('.li__span--total-stairs').text(activityUser.calculateWeeksStairsClimbed('2019/06/15', '2019/06/21', randomUserId));
-  $('.li__span--total-minutes').text(activityUser.calculateWeeksActiveMinutes('2019/06/15', '2019/06/21', randomUserId));
+  $('.li__span--total-steps').text(activityUser.calculateWeeksSteps('2019/09/16', '2019/09/22', randomUserId));
+  $('.li__span--total-stairs').text(activityUser.calculateWeeksStairsClimbed('2019/09/16', '2019/09/22', randomUserId));
+  $('.li__span--total-minutes').text(activityUser.calculateWeeksActiveMinutes('2019/09/16', '2019/09/22', randomUserId));
 
 
 
